f5eb908db04c1fe4cd5b3e1543a6a011b5b8d07e
psychokiller2144 <79776630+psychokiller2144@users.noreply.github.com>
2022-08-09 00:37:26 -0500

    Add files via upload

---
+++ Lesson9/ControlCltsInter/src/main/java/com/example/web/HomeController.java
@@ -0,0 +1,58 @@
+package com.example.web;
+
+import com.example.domain.Persona;
+import com.example.service.PersonaService;
+import javax.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+    
+    @Autowired
+    private PersonaService personaService;
+    
+    @GetMapping("/")
+    public String home(Model model){
+        
+        Iterable <Persona> personas =  personaService.listarPersonas();
+                
+        model.addAttribute("personas", personas);
+        return "index";
+    }
+    
+    @GetMapping("/agregar")
+    public String agregar(Persona persona){
+        return "modificar";
+    }
+    
+    @PostMapping("/guardar")
+    public String guardar(@Valid Persona persona, Errors errors){
+        
+        if(errors.hasErrors()){
+            return "modificar";
+        }
+        personaService.guardar(persona);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/editar/{idPersona}")
+    public String editar(Persona persona, Model model){
+        persona = personaService.encontrarPersona(persona);
+        model.addAttribute("persona", persona);
+        return "modificar";
+    }
+    
+    @GetMapping("/eliminar/{idPersona}")
+    public String eliminar(Persona persona){
+        personaService.eliminar(persona);
+        return "redirect:/";
+    }
+    
+}
